3934c25e31da3531ed6be51f2371c4865b2a87a3
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-05-14 19:11:07 +0200

    Dodanie wzorca stanów

---
+++ src/DesignPatterns/StatePattern/Order.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace StatePattern
+{
+    public class Order
+    {
+        public Order(OrderStatus initialState = OrderStatus.Processing)
+        {            
+            Status = initialState;
+        }
+
+        public OrderStatus Status { get; private set; }
+        public bool IsPaid { get; private set; }       
+
+        public void Paid()
+        {
+            IsPaid = true;
+        }
+
+        public void Confirm()
+        {
+            if (Status == OrderStatus.Pending)
+            {
+                if (IsPaid)
+                {
+                    Status = OrderStatus.Processing;
+                }                
+            }            
+            else if (Status == OrderStatus.Processing)
+            {
+                Status = OrderStatus.Completed;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+
+        }
+
+        public void Cancel()
+        {
+            if (Status == OrderStatus.Pending)
+            {
+                Status = OrderStatus.Canceled;
+            }
+            else if (Status == OrderStatus.Processing)
+            {
+                Status = OrderStatus.Canceled;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public override string ToString() => $"Order {Environment.NewLine}";
+        
+    }
+
+    public enum OrderStatus
+    {
+        // The customer places an order on the company's website
+        Pending,
+        Processing,      
+        Completed,
+        Canceled
+    }
+
+}
