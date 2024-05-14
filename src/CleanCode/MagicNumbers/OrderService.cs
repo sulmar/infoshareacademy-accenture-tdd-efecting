e708a6d5dde30c99a78659c555aa9b0fb316ed08
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-05-14 14:08:38 +0200

    Zmiana nazwy klasy

---
+++ src/CleanCode/MagicNumbers/OrderService.cs
@@ -0,0 +1,37 @@
+﻿namespace MagicNumbers;
+
+public class Order
+{
+    public double TotalAmount { get; set; }
+    public int OrderStatus { get; set; }
+
+    public double ApplyDiscount()
+    {
+        // Magiczna liczba 0.1
+        return TotalAmount * 0.9;
+    }
+}
+
+
+public class OrderService
+{
+    public void Process(Order order)
+    {
+        if (order.OrderStatus == 0)
+        {
+            // Pending
+        }
+        else if (order.OrderStatus == 1)
+        {
+            // Processing 
+        }
+        else if (order.OrderStatus == 2)
+        {
+            // Completed
+        }
+
+
+    }
+
+
+}
