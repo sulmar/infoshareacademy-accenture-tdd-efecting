05aef9adbc474fc1793ee896cd4039d6bfc3c4c0
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-05-15 11:15:54 +0200

    before

---
+++ src/CleanCode/NamingConventions/BooleanExamples.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamingConventions
+{
+    // 
+
+    internal interface BooleanExamples
+    {
+        public bool Validate(int number);
+        public bool Available();
+        public bool Active();
+        public bool Enabled();
+        public bool Open();
+        public bool Completed();
+        public bool LoggedIn();
+        public bool Edit();
+
+    }
+}
