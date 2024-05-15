6d00cf108076318a3b02d55f840af45f52354cf7
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-05-15 11:49:42 +0200

    refactor

---
+++ src/CleanCode/Comments/ListExpressions.cs
@@ -0,0 +1,7 @@
+﻿namespace Comments;
+
+public static class ListExtensions
+{
+    public static bool IsNullOrEmpty<T>(this List<T> list) => list == null || list.Count == 0;
+}
+
---
+++ src/CleanCode/Comments/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Comments;
+
+var names  = new List<string>();
+List<decimal>? salaries = null;
+
+// Iteracja po nazwach
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
+
+// Jeśli lista jest pusta wyświetlamy powiadomienie
+if (names.IsNullOrEmpty())
+{
+    Console.WriteLine("Lista rezultatów jest pusta");
+}
+
+
+if (salaries.IsNullOrEmpty())
+{
+    Console.WriteLine("Lista rezultatów jest pusta");
+}
