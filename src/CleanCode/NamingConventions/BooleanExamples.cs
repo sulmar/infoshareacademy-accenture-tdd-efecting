31ff1ed8bb8b0790324ea3fab835fdf59357dc93
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-05-15 11:24:29 +0200

    refactor

--- src/CleanCode/NamingConventions/BooleanExamples.cs
+++ src/CleanCode/NamingConventions/BooleanExamples.cs
@@ -6,18 +6,21 @@ using System.Threading.Tasks;
 
 namespace NamingConventions
 {
-    // 
+    // Metody, zmienne, pola i właściwości logiczne powinny być prefiksowane is, can, has
 
     internal interface BooleanExamples
     {
-        public bool Validate(int number);
-        public bool Available();
-        public bool Active();
-        public bool Enabled();
-        public bool Open();
-        public bool Completed();
-        public bool LoggedIn();
-        public bool Edit();
-
+        public bool IsValidNumber(int number);
+        public bool IsAvailable();
+        public bool IsActive();
+        public bool IsEnabled();
+        public bool IsOpen();
+        public bool IsCompleted();
+        public bool IsLoggedIn();
+        public bool CanEdit();
+        public bool IsEditable();
+        public bool CanChangePassword();
+        public bool HasPermission(string permission);
+        public bool HasAnyOrder();
     }
 }
