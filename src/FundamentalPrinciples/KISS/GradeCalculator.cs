47a3458f8acaaf2e20775e62852a324de8dd0d1d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-05-16 10:01:40 +0200

    refactor: linq

--- src/FundamentalPrinciples/KISS/GradeCalculator.cs
+++ src/FundamentalPrinciples/KISS/GradeCalculator.cs
@@ -4,18 +4,12 @@ public class GradeCalculator
 {
     public double CalculateAverageGrade(List<double> grades)
     {
-        if (grades == null || grades.Count == 0)
+        if (grades == null || !grades.Any())
         {
             throw new ArgumentException("Grade list cannot be null or empty.");
         }
 
-        double sum = 0;
-        foreach (var grade in grades)
-        {
-            sum += grade;
-        }
-
-        return sum / grades.Count;
+        return grades.Average();
     }
 
    
