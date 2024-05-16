e12a573a02baf1e4a0e9009a5543c42dd6f1fff7
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-05-16 13:05:09 +0200

    Dodanie przykładu złej praktyki

---
+++ src/SolidPrinciples/InterfaceSegregationPrinciple/Repository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregationPrinciple
+{    
+
+    class DbRepository : IRepository
+    {
+        public void AddCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRoute(Route route)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetCustomer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Route GetRoute(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vehicle GetVehicle(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vehicle GetVehicleByPlate(string plate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    interface IRepository
+    {
+        Customer GetCustomer(int id);
+        void AddCustomer(Customer customer);
+
+        Vehicle GetVehicle(int id);       
+        void AddVehicle(Vehicle vehicle);
+
+        Route GetRoute(int id);
+        void AddRoute(Route route);
+        Vehicle GetVehicleByPlate(string plate);
+    }
+
+    class Customer { }
+
+    class Vehicle { }
+
+    class Route { }
+
+    class RouteDetail { }
+
+    class Order { }
+
+    class OrderDetail { }
+
+    class Invoice { }
+
+    class InvoiceDetail { }
+}
