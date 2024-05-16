7151033904127b1aa3f147e726c7f37779b5da29
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-05-16 10:50:42 +0200

    refactor

--- src/SolidPrinciples/SingleResponsibilityPrinciple/Program.cs
+++ src/SolidPrinciples/SingleResponsibilityPrinciple/Program.cs
@@ -2,24 +2,58 @@
 
 Console.WriteLine("Hello, Single Responsibility Principle (SRP)!");
 
+Customer customer = new Customer();
+
 class Customer
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public Address Address { get; set; }
+    public string Email { get; set; }    
+}
+
+class Address
+{
     public string City { get; set; }
     public string PostCode { get; set; }
     public string Street { get; set; }
+    public Location Location { get; set; }
+}
+
+struct Location
+{
+    public float Latitude { get; set; }
+    public float Longitude { get; set; }
+
+    public bool IsValid()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class User
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
     public string Email { get; set; }
 
-    public void ValidateEmail(string email)
+}
+
+
+public class EmailValidator 
+{
+    public void Validate(string email)
     {
         if (!email.Contains("@") || !email.Contains("."))
         {
             throw new FormatException("Email address is a invalid format!");
         }
     }
+}
 
-    public void ValidatePostCode(string postcode)
+public class PostCodeValidator 
+{
+    public void Validate(string postcode)
     {
         if (postcode.Length != 5)
         {
